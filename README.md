bd54bc2b0e663db954871e267182ffabd94e4e02
curtrode <59448236+curtrode@users.noreply.github.com>
2024-07-20 10:08:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FSS_practice
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/curtrode/FSS_practice)
---
+++ index.js
@@ -0,0 +1,10 @@
+// // // run `node index.js` in the terminal
+
+// // console.log(`Hello Node.js v${process.versions.node}!`);
+
+
+// (function(){
+//   console.log("HELLO IIFE")
+// })();
+
+(function(num))
